edf4d36b6b109b33b11be61cae7ebc94939b0768
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-23 12:24:41 +0400

    add hover card to display identities of users

--- client/components/HeaderSimple.tsx
+++ client/components/HeaderSimple.tsx
@@ -1,6 +1,14 @@
-import { Group, Button, Text, Box } from "@mantine/core";
+import {
+  Group,
+  Button,
+  Text,
+  Box,
+  HoverCard,
+  UnstyledButton,
+} from "@mantine/core";
 import classes from "../styles/HeaderSimple.module.css";
 import { IconPaint } from "@tabler/icons-react";
+import { IconLock } from "@tabler/icons-react";
 
 export default function HeaderSimple({ playerCount }: { playerCount: number }) {
   return (
@@ -13,9 +21,26 @@ export default function HeaderSimple({ playerCount }: { playerCount: number }) {
               art98
             </Text>
           </Group>
-          <Text size="md" fw={600}>
-            {playerCount} {playerCount > 1 ? "players " : "player"} online
-          </Text>
+          <HoverCard width={280} shadow="md">
+            <HoverCard.Target>
+              <UnstyledButton>
+                {" "}
+                <Text size="md" fw={600}>
+                  {playerCount} {playerCount > 1 ? "players " : "player"} online
+                </Text>
+              </UnstyledButton>
+            </HoverCard.Target>
+            <HoverCard.Dropdown>
+              <Group>
+                <IconLock />
+                <Text size="sm">
+                  Only premium users and administrators can see the identities
+                  of online users.
+                </Text>
+              </Group>
+            </HoverCard.Dropdown>
+          </HoverCard>
+
           <Group visibleFrom="sm">
             <Button variant="default">Log out</Button>
           </Group>
