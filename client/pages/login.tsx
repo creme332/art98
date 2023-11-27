6a351dd10f2334cb9bb536ad5d4066e857faf4d9
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-27 14:57:13 +0400

    create separate register and login forms

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,5 +1,94 @@
-import AuthForm from "../components/AuthForm";
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  Button,
+  Anchor,
+  Stack,
+} from "@mantine/core";
+import { SyntheticEvent } from "react";
+import Link from "next/link";
+import { useState } from "react";
+import classes from "../styles/LoginForm.module.css";
+import { BACKEND_URL } from "../common/constants";
+import { useRouter } from "next/router";
 
-export default function LoginPage() {
-  return <AuthForm type="login"/>;
+interface loginDetails {
+  email: string;
+  password: string;
+}
+
+export default function AuthenticationForm() {
+  const router = useRouter();
+  const [values, setValues] = useState<loginDetails>({
+    email: "",
+    password: "",
+  });
+
+  async function submitHandler(e: SyntheticEvent) {
+    e.preventDefault();
+    console.log(values);
+
+    try {
+      const response = await fetch(`${BACKEND_URL}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+      const json = (await response).json();
+      console.log(json);
+    } catch (error) {}
+  }
+
+  return (
+    <div className={classes.wrapper}>
+      <Paper className={classes.form} radius="md" p="xl" withBorder>
+        <Text size="lg" fw={600}>
+          Login to art98
+        </Text>
+        <form onSubmit={submitHandler}>
+          <Stack>
+            <TextInput
+              required
+              label="Email"
+              placeholder="hello@mantine.dev"
+              radius="md"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+            />
+            <PasswordInput
+              required
+              label="Password"
+              placeholder="Your password"
+              radius="md"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
+            />
+          </Stack>
+          <Group justify="space-between" mt="xl">
+            <Anchor
+              component={Link}
+              type="button"
+              c="dimmed"
+              href={"/register"}
+              size="xs"
+            >
+              {"Don't have an account? Register"}
+            </Anchor>
+            <Button type="submit" radius="xl">
+              Login
+            </Button>
+          </Group>
+        </form>
+      </Paper>
+    </div>
+  );
 }
--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,5 +1,109 @@
-import AuthForm from "../components/AuthForm";
+import {
+  Paper,
+  TextInput,
+  PasswordInput,
+  Radio,
+  Button,
+  Title,
+  Text,
+  Group,
+  Anchor,
+} from "@mantine/core";
+import classes from "../styles/AuthForm.module.css";
+import Link from "next/link";
+import { SyntheticEvent } from "react";
+import { useState } from "react";
+import { BACKEND_URL } from "../common/constants";
 
-export default function RegisterPage() {
-  return <AuthForm type="register" />;
+interface User {
+  type: "basic" | "premium" | "admin";
+  email: string;
+  name: string;
+  password: string;
+}
+
+/**
+ * Reference: https://ui.mantine.dev/category/authentication/#authentication-image
+ * @returns
+ */
+export default function RegistrationForm() {
+  const [values, setValues] = useState<User>({
+    type: "basic",
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  async function submitHandler(e: SyntheticEvent) {
+    e.preventDefault();
+    console.log("submitted", values);
+
+    try {
+      const response = await fetch(`${BACKEND_URL}/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      const json = response.json();
+      console.log(json);
+    } catch (error) {}
+  }
+  return (
+    <div className={classes.wrapper}>
+      <Paper className={classes.form} radius={0} p={30}>
+        <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+          {"Welcome to art98!"}
+        </Title>
+
+        <form onSubmit={submitHandler}>
+          <TextInput
+            label="Email address"
+            placeholder="hello@gmail.com"
+            size="md"
+            onChange={(e) => setValues({ ...values, email: e.target.value })}
+          />
+          <TextInput
+            onChange={(e) => setValues({ ...values, name: e.target.value })}
+            label="Name"
+            placeholder="Your name"
+            size="md"
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            mt="md"
+            size="md"
+            onChange={(e) => setValues({ ...values, password: e.target.value })}
+          />
+          <Radio.Group
+            size="md"
+            mt="md"
+            name="type"
+            label="Select your role"
+            onChange={(e: "basic" | "premium" | "admin") =>
+              setValues({ ...values, type: e })
+            }
+          >
+            <Group mt="xs">
+              <Radio checked value="basic" label="Basic" />
+              <Radio value="premium" label="Premium" />
+              <Radio value="admin" label="Administrator" />
+            </Group>
+          </Radio.Group>
+          <Button type="submit" fullWidth mt="xl" size="md">
+            {"Register"}
+          </Button>
+        </form>
+
+        <Text ta="center" mt="md">
+          {"Already have an account?"}
+          <Anchor component={Link} href={"/login"} fw={700}>
+            {" Login"}
+          </Anchor>
+        </Text>
+      </Paper>
+    </div>
+  );
 }
