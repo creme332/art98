5634b36e1feb197e3e9f72903f5153b33858603c
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-28 14:15:31 +0400

    remove  redirect after validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,17 +6,23 @@ const signUpController = require("../controllers/sign_up_controller");
 router.post("/register", signUpController.create_new_user);
 
 /* Authenticate users login */
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureRedirect: false,
-    failureMessage: true,
-  }),
-  (req, res) => {
-    // console.log(req);
-    return res.json({ message: "Success" });
-  }
-);
+router.post("/login", function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
+    // console.log(err, user, info);
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).send(info);
+    }
+    req.login(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      return res.status(200).send(user);
+    });
+  })(req, res, next);
+});
 
 /* Deal with user log out*/
 router.post("/logout", function (req, res, next) {
