de0c4c727ea42d0875367f2695238b2a7e39093b
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-29 18:27:30 +0400

    ask user for secret key if needed, fix other issues

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -14,12 +14,10 @@ import Link from "next/link";
 import { SyntheticEvent } from "react";
 import { useState } from "react";
 import { BACKEND_URL } from "../common/constants";
+import { User } from "../common/types";
 
-interface User {
-  type: "basic" | "premium" | "admin";
-  email: string;
-  name: string;
-  password: string;
+interface specialUser extends User {
+  secret?: string;
 }
 
 /**
@@ -27,7 +25,7 @@ interface User {
  * @returns
  */
 export default function RegistrationForm() {
-  const [values, setValues] = useState<User>({
+  const [values, setValues] = useState<specialUser>({
     type: "basic",
     email: "",
     password: "",
@@ -36,7 +34,8 @@ export default function RegistrationForm() {
 
   async function submitHandler(e: SyntheticEvent) {
     e.preventDefault();
-    console.log("submitted", values);
+
+    console.log("Submitted: ", values);
 
     try {
       const response = await fetch(`${BACKEND_URL}/auth/register`, {
@@ -46,6 +45,12 @@ export default function RegistrationForm() {
         },
         body: JSON.stringify(values),
       });
+
+      if (response.ok) {
+        window.alert("Registration successful! You may now login.");
+        return;
+      }
+      // else an error occurred
       const json = await response.json();
       window.alert(json.error);
     } catch (error) {
@@ -61,18 +66,21 @@ export default function RegistrationForm() {
 
         <form onSubmit={submitHandler}>
           <TextInput
+            required
             label="Email address"
             placeholder="hello@gmail.com"
             size="md"
             onChange={(e) => setValues({ ...values, email: e.target.value })}
           />
           <TextInput
+            required
             onChange={(e) => setValues({ ...values, name: e.target.value })}
             label="Name"
             placeholder="Your name"
             size="md"
           />
           <PasswordInput
+            required
             label="Password"
             placeholder="Your password"
             mt="md"
@@ -80,20 +88,34 @@ export default function RegistrationForm() {
             onChange={(e) => setValues({ ...values, password: e.target.value })}
           />
           <Radio.Group
+            required
             size="md"
             mt="md"
             name="type"
             label="Select your role"
+            defaultValue="Basic"
             onChange={(e: "basic" | "premium" | "admin") =>
               setValues({ ...values, type: e })
             }
           >
             <Group mt="xs">
-              <Radio checked value="basic" label="Basic" />
-              <Radio value="premium" label="Premium" />
-              <Radio value="admin" label="Administrator" />
+              <Radio checked value="Basic" label="Basic" />
+              <Radio value="Premium" label="Premium" />
+              <Radio value="Admin" label="Administrator" />
             </Group>
           </Radio.Group>
+
+          {/* If user's role is set to premium or administrator, ask for secret */}
+          {values.type !== "basic" && (
+            <TextInput
+              mt="md"
+              required
+              label="Secret key"
+              placeholder="Enter secret key"
+              size="md"
+              onChange={(e) => setValues({ ...values, secret: e.target.value })}
+            />
+          )}
           <Button type="submit" fullWidth mt="xl" size="md">
             {"Register"}
           </Button>
