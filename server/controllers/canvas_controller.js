d0a6276098c9b6a68ea1032e3a283ffeb29b7c11
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-29 21:42:47 +0400

    add route that returns pixel colors sorted by position

---
+++ server/controllers/canvas_controller.js
@@ -0,0 +1,51 @@
+const asyncHandler = require("express-async-handler");
+const Canvas = require("../models/canvas");
+
+exports.pixels_color = asyncHandler(async (req, res, next) => {
+  const canvas = await Canvas.findOne(); // There's only 1 canvas in database
+  const pixels = await Canvas.aggregate([
+    {
+      // perform aggregation on a single canvas
+      $match: { _id: canvas._id },
+    },
+    {
+      // replace each object id in pixels array by actual pixel dictionary
+      $lookup: {
+        from: "pixels",
+        localField: "pixels",
+        foreignField: "_id",
+        as: "pixels",
+      },
+    },
+    {
+      // remove canvas id from result
+      $project: { _id: 0 },
+    },
+    {
+      // sort pixels array in ascending order of position
+      $project: {
+        _id: 0,
+        pixels: {
+          $sortArray: {
+            input: "$pixels",
+            sortBy: { position: 1 },
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        "pixels.color": 1,
+      },
+    },
+  ]);
+  /*
+  Result from mongodb looks like this:
+  [{"pixels":[{"color":"white"},{"color":"white"},{"color":"white"},{"color":"white"}]}]
+  
+  We want: ["white","white","white","white"]
+  */
+  // return an array of colors
+  const final = pixels[0].pixels.map((el) => el.color);
+  res.json(final);
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const canvasController = require("../controllers/canvas_controller");
 
 router.get("/", function (req, res, next) {
   res.json({ message: "Hello world" });
 });
+
+router.get("/canvas", canvasController.pixels_color);
 
 module.exports = router;
