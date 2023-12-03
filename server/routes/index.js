8e3a5b73ed95a14d21f706bdb5aa9a1272ab9b4d
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-03 11:27:54 +0400

    add new routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const canvasController = require("../controllers/canvas_controller");
+const userController = require("../controllers/user_controller");
 
 router.get("/", (req, res, next) => {
   res.json({ message: "Hello world" });
 });
 
 router.get("/canvas", canvasController.pixels_color);
+router.get("/canvas/reset", canvasController.reset_canvas);
+router.get("/user", userController.user_data);
 
 module.exports = router;
