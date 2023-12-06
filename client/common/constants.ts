c1bba419693de887dcc1be0341896cb7856c4e6d
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-06 11:48:05 +0400

    update backend url and add demo account credentials

--- client/common/constants.ts
+++ client/common/constants.ts
@@ -1,7 +1,18 @@
 const env = process.env.NODE_ENV;
 export const BACKEND_URL =
-  env === "development" ? "http://localhost:4000" : "https://art98.vercel.app";
+  env === "development" ? "http://localhost:4000" : "https://art98-backend.onrender.com";
 
+/**
+ * Login details for demo account
+ */
+export const demoAccountCredentials = {
+  email: process.env.DEMO_EMAIL || "error",
+  password: process.env.DEMO_PASSWORD || "error",
+};
+
+/**
+ * Hex colors for color palette
+ */
 export const canvasColors = [
   "#000000",
   "#FFFFFF",
