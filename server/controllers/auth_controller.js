f37d2b8d174e26671ad6c4b91ad7331495067019
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-06 17:19:49 +0400

    refactor create new user

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
@@ -12,6 +13,7 @@ const {
   emailChain,
 } = require("../middlewares/validator");
 const passport = require("passport");
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS, 10);
 
 exports.create_new_user = [
   uniqueEmailChain(),
@@ -22,25 +24,19 @@ exports.create_new_user = [
   confirmPasswordChain(),
   validateChains, // ! validate must be at the end of the chain
   asyncHandler(async (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-      if (err) {
-        console.log(err);
-      } else {
-        try {
-          const user = new User({
-            email: req.body.email,
-            name: req.body.name,
-            password: hashedPassword,
-            type: req.body.type,
-          });
-          const result = await user.save();
-          console.log(result);
-          return res.status(200).send();
-        } catch (err) {
-          return res.status(401).send({ error: JSON.stringify(err) });
-        }
-      }
+    const hashedPassword = await bcrypt.hash(req.body.password, SALT_ROUNDS);
+    const user = new User({
+      email: req.body.email,
+      name: req.body.name,
+      password: hashedPassword,
+      type: req.body.type,
     });
+    try {
+      await user.save();
+      return res.status(200).send();
+    } catch (err) {
+      return res.status(401).send({ error: JSON.stringify(err) });
+    }
   }),
 ];
 
