9c392f37bef049578f2fe54e6ac25a7c289a75da
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-15 11:58:47 +0400

    add cookie information in dialog and add clear link to repository

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,18 @@
-import { Title, Text, Button, Container, Alert, Anchor } from "@mantine/core";
+import {
+  Title,
+  Text,
+  Button,
+  Container,
+  Anchor,
+  Dialog,
+} from "@mantine/core";
 import { Dots } from "../components/Dots";
 import classes from "../styles/HeroText.module.css";
 import Link from "next/link";
 import { loginDetails } from "../common/types";
 import { demoAccountCredentials } from "../common/constants";
 import { useState } from "react";
+import { useDisclosure } from "@mantine/hooks";
 
 interface pageProps {
   loginHandler: (details: loginDetails) => void;
@@ -12,6 +20,7 @@ interface pageProps {
 
 export default function HomePage({ loginHandler }: pageProps) {
   const [loading, setLoading] = useState(false);
+  const [opened, { open, close }] = useDisclosure(true);
 
   async function loginToDemo() {
     setLoading(true);
@@ -21,6 +30,30 @@ export default function HomePage({ loginHandler }: pageProps) {
 
   return (
     <div className={classes.root}>
+      <Dialog
+        opened={opened}
+        withCloseButton
+        onClose={close}
+        size="lg"
+        radius="md"
+      >
+        <Text size="sm" mb="xs" fw={500}>
+          Cookies
+        </Text>
+        <Text size="sm" mb="xs">
+          This website uses cookies to make requests to the server. If cookies
+          are disabled on your browser or if you are using an incognito tab, you
+          will not be able to login. If you face login problems, create an issue
+          on{" "}
+          <Anchor
+            fw={800}
+            fz={"xs"}
+            href="https://github.com/creme332/art98/issues"
+          >
+            {`Github.`}
+          </Anchor>
+        </Text>{" "}
+      </Dialog>
       <Container className={classes.wrapper}>
         <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
         <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
@@ -36,23 +69,16 @@ export default function HomePage({ loginHandler }: pageProps) {
           </Title>
           <Container p={0} size={600}>
             <Text size="lg" c="dimmed" className={classes.description}>
-              Inspired by r/place, this open-source project supports different
-              users with varying privileges.
-            </Text>
-            <Alert mt={40} variant="light" title="Note" color="violet">
-              This website uses cookies to make requests to the server. If
-              cookies are disabled on your browser or if you are using an
-              incognito tab, you will not be able to login. If you face login
-              problems, create an issue on{" "}
+              Inspired by r/place, this{" "}
               <Anchor
                 fw={800}
-                c={"violet"}
-                fz={"xs"}
-                href="https://github.com/creme332/art98/issues"
+                href="https://github.com/creme332/art98/"
+                className={classes.highlight}
               >
-                {`Github.`}
-              </Anchor>
-            </Alert>
+                {`open-source`}
+              </Anchor>{" "}
+              project supports different users with varying privileges.
+            </Text>
           </Container>
           <div className={classes.controls}>
             <Button
